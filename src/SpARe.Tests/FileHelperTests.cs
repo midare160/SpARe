b1ee4270d83ea4acdc11c871a971932295ff39f3
midare160 <43279447+midare160@users.noreply.github.com>
2021-02-27 20:34:56 +0100

    Add unit tests

---
+++ src/SpARe.Tests/FileHelperTests.cs
@@ -0,0 +1,34 @@
+using Spare.Helpers;
+using System.IO;
+using Xunit;
+
+namespace SpARe.Tests
+{
+    public class FileHelperTests
+    {
+        [Fact]
+        public void DeleteWhiteSpaceFile()
+        {
+            var deletionResult = FileHelper.Delete(" ");
+
+            Assert.False(deletionResult);
+        }
+
+        [Fact]
+        public void DeleteInvalidFile()
+        {
+            var deletionResult = FileHelper.Delete(@"F:\Test");
+
+            Assert.False(deletionResult);
+        }
+
+        [Fact]
+        public void DeleteTempFile()
+        {
+            var tempFile = Path.GetTempFileName();
+            var deletionResult = FileHelper.Delete(tempFile);
+
+            Assert.True(deletionResult && !File.Exists(tempFile));
+        }
+    }
+}
